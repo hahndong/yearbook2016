3d583181593d2c5e4b205c03e1946d29281b3369
hahndong <Hahn@Dongs-MacBook-Pro-2.local>
2016-04-30 11:06:15 +0800

    indexprint

--- app/controllers/solos_controller.rb
+++ app/controllers/solos_controller.rb
@@ -4,7 +4,7 @@ class SolosController < ApplicationController
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   protect_from_forgery with: :exception
   before_action :authenticate_user!, except: [:index]
-  after_action :verify_authorized, except: [:index, :designs, :volunteer, :thankyou]
+  after_action :verify_authorized, except: [:index, :indexprint, :designs, :volunteer, :thankyou]
   before_action :set_solo, only: [:show, :edit, :update, :destroy, :large]
 
   def index
